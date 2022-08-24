2102227230d50edda6116bde9fc002ff9e169a45
Paul Gelario <paul.david.gelario@gmail.com>
2022-08-25 05:55:16 +0800

    adding these

---
+++ LeBron_Cabron_input.py
@@ -0,0 +1,11 @@
+def Cabron():
+    LeBron = input("What is LeBron James? \n ")
+    LeBron = LeBron.lower()
+    if LeBron == "lebron":
+        print("Correct!")
+    elif LeBron == "cabron":
+        print("Correct!")
+    else:
+        print(f" Your answer is {LeBron} Incorrect answer!")
+
+Cabron()
